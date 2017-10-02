3e35fec771853351b67325e5729ea4648b26e1fd
Ronel Girat <r.girat@tikalk.com>
2017-10-02 14:30:52 +0300

    more work on data

---
+++ app/src/main/java/com/knews/android/util/UrlExt.kt
@@ -0,0 +1,8 @@
+package com.knews.android.util
+
+/**
+ * Created by ronelg on 10/2/17.
+ */
+fun String.url(icon:String): String {
+    return "https://icons.better-idea.org/icon?url=${icon}&amp;size=70..120..200"
+}
