14e0df9ebd93874f707b33773245e899399e8a44
Ronel Girat <r.girat@tikalk.com>
2017-10-02 15:24:01 +0300

    more work start room db

--- app/src/main/java/com/knews/android/data/Source.kt
+++ app/src/main/java/com/knews/android/data/Source.kt
@@ -1,27 +1,45 @@
 package com.knews.android.data
 
+import android.arch.persistence.room.ColumnInfo
+import android.arch.persistence.room.Entity
+import android.arch.persistence.room.Ignore
+import android.arch.persistence.room.PrimaryKey
 import com.google.gson.annotations.SerializedName
 
 /**
  * Created by ronelg on 10/2/17.
  */
+@Entity(tableName = "sources")
 data class Source(
         @SerializedName("id")
+        @PrimaryKey
+        @ColumnInfo(name = "id")
         val id: String,
         @SerializedName("name")
+        @ColumnInfo(name = "name")
         val name: String? = null,
         @SerializedName("description")
+        @ColumnInfo(name = "description")
         val description: String? = null,
         @SerializedName("url")
+        @ColumnInfo(name = "url")
         val url: String? = null,
         @SerializedName("category")
+        @ColumnInfo(name = "category")
         val category: String? = null,
         @SerializedName("language")
+        @ColumnInfo(name = "language")
         val language: String? = null,
         @SerializedName("country")
+        @ColumnInfo(name = "country")
         val country: String? = null,
+        @Ignore
         @SerializedName("sortBysAvailable")
         val sortBysAvailable: List<SortBy>? = null) {
+
+    fun getLogo(): String {
+        return "https://icons.better-idea.org/icon?url=$url&amp;size=70..120..200"
+    }
 }
 
 enum class SortBy {
